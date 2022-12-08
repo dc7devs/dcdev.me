95377e1d7e71b1863c2903fce5da07a6d11dfbee
Diego C. Silva <diegocaetano444@outlook.com>
2022-12-08 05:11:06 -0300

    readjusted layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import B from '../components/background'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <B>
       <Component {...pageProps} />
+    </B>
   )
 }
