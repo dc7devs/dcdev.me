f750640cdcad5340ad5f31e6f768680be7f96211
Diego C. Silva <diegocaetano444@outlook.com>
2022-12-08 06:19:10 -0300

    Refactored typing with 'ReactNode'

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,4 +1,10 @@
-export default function Bg({children}: any) {
+import { ReactNode } from "react"
+
+type PropsChildren = {
+    children: ReactNode
+}
+
+export default function Bg({children}: PropsChildren) {
     return(
         <div className="h-screen bg-gray/[.7] bg-paper-pattern bg-paper-pattern-size bg-paper-pattern-position">
             {children}
