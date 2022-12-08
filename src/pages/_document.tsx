c5cea2a05b300fa882438789d01d5f0167e0b3cc
Diego C. Silva <diegocaetano444@outlook.com>
2022-12-08 05:05:26 -0300

    added file _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+    return () {
+        <Html lang="pt-BR">
+            <Head>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    }
+}
