8aee4be865ed6781eef7c5fee9dba8a0ad67a1be
Diego C. Silva <diegocaetano444@outlook.com>
2022-12-09 22:38:30 -0300

    adjustments in responsiveness

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,8 +1,10 @@
+import { Link } from "phosphor-react";
+
 export default function Modal() {
     return (
         <div className="fixed top-0 left-0 w-screen h-screen bg-white/10 backdrop-blur-sm z-40 transition-opacity ease-in-out">
             <div>
-
+                <h1>Ola mundo</h1>
             </div>
         </div>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,24 +5,24 @@ import Layout from '../components/layout'
 export default function Home() {
   return (
       <Layout>
-        <div className="w-full h-full flex items-center">
-          <div className="flex flex-col gap-3">
+        <div className="sm:w-full sm:h-full flex mt-24 sm:m-0 sm:items-center">
+          <div className="flex flex-col gap-10 md:gap-5 px-4">
             <h2 className="flex flex-col text-zinc">
               <div className="text-4xl font-semibold ">
                 Hi!, my name is
               </div>
-              <div className="text-7xl pl-5 font-extralight">
+              <div className="text-5xl md:text-7xl md:ml-5 font-extralight">
                 DIEGO SILVA
               </div>
             </h2>
             <p 
-              className="text-xl pl-5"
+              className="text-xl md:ml-5"
             >I'm a software <strong>
               <Link href="https://www.google.com/search?q=what+is+developer" className="underline" target="__blank">
                 developer
               </Link>
             </strong>!</p>
-            <button className="max-w-max h-8 px-2 text-xl font-regular rounded-lg text-black bg-black/25 ml-5 shadow-lg shadow-violet-700/25 bakcdrop-blur transition ring-1 ring-violet-700/50">
+            <button className="max-w-max h-8 px-2 text-xl font-regular rounded-lg text-black bg-black/25 md:ml-5 shadow-lg shadow-violet-700/25 bakcdrop-blur transition ring-1 ring-violet-700/50">
               <Link href="/about">
                 more...
               </Link>
