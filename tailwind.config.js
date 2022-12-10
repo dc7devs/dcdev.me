d57b5a9a86069a5d39fb343a514beae4873faf2b
Diego C. Silva <diegocaetano444@outlook.com>
2022-12-09 21:05:19 -0300

    Added simple description about me

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,21 +11,7 @@ module.exports = {
         separator: '#3F3F3F',
 
         black: '#161616',
-        'black-10': 'rgba(22, 22, 22, .1)',
-        'black-25': 'rgba(22, 22, 22, .25)',
-        'black-35': 'rgba(22, 22, 22, .35)',
-        'black-50': 'rgba(22, 22, 22, .5)',
-        'black-70': 'rgba(22, 22, 22, .7)',
-        'black-80': 'rgba(22, 22, 22, .8)',
-        'black-90': 'rgba(22, 22, 22, .9)',
-
         white: '#FFFFFF',
-        'white-10': 'rgba(255, 255, 255, .10)',
-        'white-25': 'rgba(255, 255, 255, .25)',
-        'white-35': 'rgba(255, 255, 255, .35)',
-        'white-70': 'rgba(255, 255, 255, .7)',
-        'white-80': 'rgba(255, 255, 255, .8)',
-        'white-90': 'rgba(255, 255, 255, .9)',
 
         gray: '#AEAEAE'
       },
