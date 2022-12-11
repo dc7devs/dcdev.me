7f00ae81d3192dbb90f16921e7e60fb3203a60d4
Diego C. Silva <diegocaetano444@outlook.com>
2022-12-11 13:06:30 -0300

    Added intl

---
+++ src/intl/HomePage.ts
@@ -0,0 +1,32 @@
+export const LangString = {
+    en: {
+        home: 'Home',
+        about: 'About',
+        projects: 'Projects',
+        contact: 'Contact',
+        apresentation: {
+            hi: 'Hi! my name is',
+            name: 'DIEGO SILVA',
+            job: {
+                msg: "I'm a software",
+                jobName: "developer!"
+            },
+            aboutButton: "about me..."
+        }
+    },
+    pt: {
+        home: 'Inicio',
+        about: 'Sobre',
+        projects: 'Projetos',
+        contact: 'Contato',
+        apresentation: {
+            hi: 'Oi! meu nome e',
+            name: 'DIEGO SILVA',
+            job: {
+                msg: "Eu sou um",
+                jobName: "desenvolvedor de software!"
+            },
+            aboutButton: "mais sobre min"
+        }
+    }
+}
