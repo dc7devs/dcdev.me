2465fac0f659ee4288eeb66baf27b251f3ed8b24
Diego C. Silva <diegocaetano444@outlook.com>
2024-02-03 01:29:09 -0300

    feat: added tooltip component

---
+++ components/tooltip.tsx
@@ -0,0 +1,33 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from '@/components/ui/tooltip';
+import { cn } from '@/lib/utils';
+import { ReactNode } from 'react';
+
+interface ToolTipProps {
+  children: ReactNode;
+  message: string;
+  className?: string;
+}
+
+export default function ToolTip({
+  children,
+  message,
+  className
+}: ToolTipProps) {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent
+          className={cn('px-2 py-1 backdrop-brightness-50', className)}
+        >
+          <p>{message}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
