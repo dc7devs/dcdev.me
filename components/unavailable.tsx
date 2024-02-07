dcc78d3245b99d869c6461e1e67c30dfb23171e0
Diego C. Silva <diegocaetano444@outlook.com>
2024-02-06 21:22:39 -0300

    style: adjustments to unavailable component

--- components/unavailable.tsx
+++ components/unavailable.tsx
@@ -1,14 +1,18 @@
+import { cn } from '@/lib/utils';
 import Icons from './ui/icons';
 
-export default function Unavailable() {
+export default function Unavailable({ className }: { className?: string }) {
   return (
     <div
-      className="flex items-center justify-center gap-2 absolute top-0 left-0 w-full h-full bg-white/50 dark:bg-black/50 text-foreground/60 rounded-xl select-none cursor-not-allowed"
+      className={cn(
+        'flex items-center justify-center gap-2 absolute top-0 left-0 w-full h-full bg-white/50 dark:bg-black/50 text-foreground/60 rounded-xl select-none cursor-not-allowed',
+        className
+      )}
       style={{ backdropFilter: 'blur(2px)' }}
     >
-      <Icons.FluentEmojiHighContrastMagnifyingGlassTiltedLeft />
+      <Icons.IonIosHammer />
       <small>
-        <strong>Unavailable!</strong>
+        <strong>unavailable, building...</strong>
       </small>
     </div>
   );
