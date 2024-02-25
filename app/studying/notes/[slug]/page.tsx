4fb56f74aacf422515de14b1e97ae7111a3c9fa5
Diego C. Silva <diegocaetano444@outlook.com>
2024-02-24 21:48:16 -0300

    feat: implementing the notes page

--- app/studying/notes/[slug]/page.tsx
+++ app/studying/notes/[slug]/page.tsx
@@ -1,3 +1,44 @@
-export default function NotePage({ params }: { params: { slug: string } }) {
-  return <h3>{params.slug}</h3>;
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+
+import { allNotes } from '@/.contentlayer/generated';
+import { getMDXComponent } from 'next-contentlayer/hooks';
+
+import BackButton from '@/components/go-back-button';
+import Icons from '@/components/ui/icons';
+
+export default function NotePage({
+  params: { slug }
+}: {
+  params: { slug: string };
+}) {
+  const formattedSlug = `/studying/notes/${slug}`;
+
+  const note = allNotes.find(
+    (note) => note.slug.trim() == formattedSlug.trim()
+  );
+
+  if (!note) return;
+  const Content = getMDXComponent(note.body.code);
+
+  return (
+    <div className="w-full pt-14 sm:pt-10 ">
+      <article className="max-w-3xl mx-auto prose prose-zinc dark:prose-invert prose-p:leading-normal relative">
+        <BackButton className="absolute -top-11 -left-1 lg:top-8 lg:-left-24 xl:-left-32 text-muted-foreground hover:text-foreground">
+          <Icons.PhArrowCircleLeftDuotone className="size-5/6 lg:size-3/4" />
+        </BackButton>
+
+        <div className="mb-8">
+          <time dateTime={note.createdAt} className="mb-1 text-sm">
+            {format(parseISO(note.createdAt), 'LLLL d, yyyy', {
+              locale: ptBR
+            })}
+          </time>
+          <h1>{note.title}</h1>
+        </div>
+
+        <Content />
+      </article>
+    </div>
+  );
 }
---
+++ app/studying/notes/page.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+
+import { allNotes } from '@/.contentlayer/generated';
+import { NoteCard } from '@/components/note-card';
+
+import BackButton from '@/components/go-back-button';
+import Icons from '@/components/ui/icons';
+
+export default function NotesPage() {
+  return (
+    <div className="w-full pt-14 sm:pt-10">
+      <div className="prose prose-zinc dark:prose-invert prose-p:leading-normal prose-p:my-3 mx-auto text-center">
+        <h1 className="text-black dark:text-white font-medium text-xl md:px-2 rounded-sm md:bg-accent max-w-max mx-auto">
+          All Notes
+        </h1>
+
+        <p>
+          Here are all my little notes on study content, techniques I use in a
+          specific language or tech, on the programming environment, books,
+          videos, tools and more!
+        </p>
+      </div>
+
+      <BackButton className="flex text-muted-foreground hover:text-foreground mb-7">
+        <Icons.PhArrowCircleLeftDuotone className="size-3/4" />
+      </BackButton>
+
+      <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2.5 auto-rows-auto">
+        {allNotes.map((note, index) => (
+          <Link key={index} href={note.slug}>
+            <NoteCard {...note} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
