202fff7f61ca5e108609f38bfeb588ccfbc2ef27
Diego C. Silva <diegocaetano444@outlook.com>
2024-03-03 09:47:03 -0300

    feat: implementation of project showcase component

---
+++ app/projects/all/page.tsx
@@ -0,0 +1,80 @@
+import { Project, allProjects } from '@/.contentlayer/generated';
+import BackButton from '@/components/go-back-button';
+import { Cards } from '@/components/projects';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from '@/components/ui/carousel';
+import Icons from '@/components/ui/icons';
+
+export default function AllProjectsPage() {
+  return (
+    <div className="w-full pt-14 sm:pt-10">
+      <div className="prose prose-zinc dark:prose-invert prose-p:leading-normal prose-p:my-3 mx-auto text-center">
+        <h1 className="text-black dark:text-white font-medium text-xl md:px-2 rounded-sm md:bg-accent max-w-max mx-auto">
+          Projects I&#39;m working on
+        </h1>
+
+        <p>Here are all my projects, listed and categorized by type!</p>
+      </div>
+
+      <BackButton className="flex text-muted-foreground hover:text-foreground p-0 align-middle gap-1">
+        <Icons.MaterialSymbolsArrowLeftAltRounded />
+        <small className="text-sm">back</small>
+      </BackButton>
+
+      <SectionBuilderByProjectType />
+    </div>
+  );
+}
+
+// projetos por tipo
+const SectionBuilderByProjectType = () => {
+  const organizedProjectsByType = ((projects: Array<Project>) => {
+    const organizedProjects: {
+      [key: string]: Array<Project>;
+    } = {};
+
+    projects.forEach((project) => {
+      const projectType = project.projectType.toLowerCase();
+
+      organizedProjects[projectType] = organizedProjects[projectType] || [];
+      organizedProjects[projectType].push(project);
+    });
+
+    return organizedProjects;
+  })(allProjects);
+
+  return (
+    <div>
+      {Object.keys(organizedProjectsByType).map((projectType) => (
+        <section key={projectType}>
+          <h2 className="text-black dark:text-white mb-3 mt-10 text-base font-medium">
+            {projectType}
+          </h2>
+
+          <Carousel opts={{ slidesToScroll: 'auto' }} className="w-full">
+            <CarouselContent className="-ml-3">
+              {organizedProjectsByType[projectType].map(
+                (propsProject: Project) => (
+                  <CarouselItem
+                    key={propsProject._id}
+                    className="pl-3 basis-10/12 sm:basis-3/6 md:basis-1/2 lg:basis-auto"
+                  >
+                    <Cards.ProjectCardByType {...propsProject} />
+                  </CarouselItem>
+                )
+              )}
+            </CarouselContent>
+
+            <CarouselPrevious className="left-1 sm:-left-5 bg-secondary" />
+            <CarouselNext className="right-1 sm:-right-5 bg-secondary" />
+          </Carousel>
+        </section>
+      ))}
+    </div>
+  );
+};
