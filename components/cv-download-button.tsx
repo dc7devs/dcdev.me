fb1d0831026b48fbf80b00e14e64a66e3c09e61f
Diego C. Silva <diegocaetano444@outlook.com>
2025-02-10 19:44:44 -0300

    fix(prettier): update cv-download-button.tsx

--- components/cv-download-button.tsx
+++ components/cv-download-button.tsx
@@ -1,17 +1,17 @@
 'use client';
 import Link from 'next/link';
 import Icons from './ui/icons';
-import { useTheme } from 'next-themes';
+// import { useTheme } from 'next-themes';
 import { Button } from './ui/button';
 
 export default function DownloadCV() {
   // const { theme } = useTheme();
-  const cv_url = '/assets/cv/DIego-C-Silva-CV - Estágio.pdf'
-    // theme === 'dark'
-    //   ? '/assets/cv/Diego-C-Silva-CV-EN-Dark.pdf'
-    //   : theme === 'light'
-    //     ? '/assets/cv/Diego-C-Silva-CV-EN-Light.pdf'
-    //     : '/assets/cv/Diego-C-Silva-CV-EN-Light.pdf';
+  const cv_url = '/assets/cv/DIego-C-Silva-CV - Estágio.pdf';
+  // theme === 'dark'
+  //   ? '/assets/cv/Diego-C-Silva-CV-EN-Dark.pdf'
+  //   : theme === 'light'
+  //     ? '/assets/cv/Diego-C-Silva-CV-EN-Light.pdf'
+  //     : '/assets/cv/Diego-C-Silva-CV-EN-Light.pdf';
 
   return (
     <Link className={'grow'} href={cv_url} target="_blank">
